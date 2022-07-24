60aeecc5cac03057978c84e2c5402a6eb8360473
Andre Luis <andre.1357.luis@gmail.com>
2022-07-24 03:47:46 -0300

    🐛 fix url

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 IMAGE_URL="https://i.pinimg.com/736x/78/a8/dc/78a8dc846a3a82124456ec4e9a38b7a9.jpg"
 BACKGROUND_URL="https://images7.alphacoders.com/116/thumb-1920-1168264.png"
 
-curl "http://localhost:3712/joinImages?img=$IMAGE_URL&$BACKGROUND_URL"
+curl "http://localhost:3712/joinImages?img=$IMAGE_URL&background=$BACKGROUND_URL"
