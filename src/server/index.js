5d944eaffc781342b0741f18bc3699a05efca27e
Andre Luis <andre.1357.luis@gmail.com>
2022-07-24 02:48:10 -0300

    ⚗️ http interceptor

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,22 @@
-import { createServer } from 'node:http';
+import http from 'node:http';
+import { logger } from '../support/logger/service.js';
 
-export const buildServer = async () => {
-  return createServer;
+function InjectHttpInterceptor() {
+  const oldEmit = http.Server.prototype.emit;
+
+  http.Server.prototype.emit = function (...args) {
+    const [type, request] = args;
+
+    if (type === 'request') {
+      Object.assign(request, { log: logger });
+    }
+
+    return oldEmit.apply(this, args);
+  };
+}
+
+const buildServer = async () => {
+  return http.createServer;
 };
+
+export { buildServer, InjectHttpInterceptor };
