bafbb28304eed39cf25fa93c7c5a846013acc289
Andre Luis <andre.1357.luis@gmail.com>
2022-07-26 23:25:44 -0300

    🥅 remove catch

--- src/domains/handler/processImage.js
+++ src/domains/handler/processImage.js
@@ -1,16 +1,9 @@
 import { joinImages } from '../workerThread/joinImages_piscina.js';
-// import { joinImages } from '../workerThread/joinImages.js';
 
 export async function imageProcess({ image, background, req, res }) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
 
-  const imageBase64 = await joinImages(req.log, { image, background }).catch(
-    err => {
-      req.log.error(err.message);
-
-      return new Error(new TypeError('🧵 Worker: error'));
-    }
-  );
+  const imageBase64 = await joinImages(req.log, { image, background });
 
   res.end(`
   <div style="display: grid; place-items: center;">
